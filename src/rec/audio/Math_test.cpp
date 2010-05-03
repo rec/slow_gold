c7e8422eb2fafdf61c507abdbeb7db2c32de7b2c
Tom Swirly <tom@swirly.com>
2010-05-03 14:47:59 -0400

    more Math_test.cpp - note one test disabled

--- src/rec/audio/Math_test.cpp
+++ src/rec/audio/Math_test.cpp
@@ -6,7 +6,12 @@ namespace audio {
 namespace math {
 
 TEST(RecAudio, RampWave) {
-  EXPECT_EQ(rampWave(0, 100), 0.0);
+  EXPECT_FLOAT_EQ(rampWave(0, 100), 0.0);
+  EXPECT_FLOAT_EQ(rampWave(1, 100), 0.01);
+  EXPECT_FLOAT_EQ(rampWave(100, 100), 1.0);
+  EXPECT_FLOAT_EQ(rampWave(101, 100), -1.0);
+  EXPECT_FLOAT_EQ(rampWave(102, 100), -0.99);
+  EXPECT_FLOAT_EQ(rampWave(201, 100), 0.0);
 }
 
 }  // namespace math
