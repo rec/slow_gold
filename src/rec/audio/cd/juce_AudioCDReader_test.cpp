5a1f9755a43af681bccbe3ff5bcd03c0d42dae41
Tom Swirly <tom@swirly.com>
2010-07-15 12:22:15 -0400

    Unit tests for cddb recognizer complete.

--- src/rec/audio/cd/juce_AudioCDReader_test.cpp
+++ src/rec/audio/cd/juce_AudioCDReader_test.cpp
@@ -7,15 +7,18 @@
 
 #include "rec/base/scoped_ptr.h"
 
+// #include "native/mac/juce_mac_AudioCDReader_helpers.h"
+
 #ifdef __JUCE_MAC_AUDIOCDREADER_HELPERS_JUCEHEADER__
 
 // Don't run this test unless we have the upated JUCE.
 
-TEST(AudioCDReaderHelper, getValueForKey) {
+TEST(AudioCDReaderHelper, getElementForKey) {
   XmlDocument doc("<dict><key>foo</key><value>bar</value></dict>");
   scoped_ptr<XmlElement> xml(doc.getDocumentElement());
-  EXPECT_TRUE(!getValueForKey(*xml, "bar"));
-  EXPECT_EQ(getValueForKey(*xml, "foo")->getFirstChildElement()->getText(), "bar");
+  EXPECT_TRUE(!getElementForKey(*xml, "bar"));
+  EXPECT_EQ(getElementForKey(*xml, "foo")->getFirstChildElement()->getText(),
+            "bar");
 }
 
 TEST(AudioCDReaderHelper, getIntValueForKey) {
@@ -25,16 +28,37 @@ TEST(AudioCDReaderHelper, getIntValueForKey) {
   EXPECT_EQ(getIntValueForKey(*xml, "foo"), 123);
 }
 
+namespace {
+
+// http://www.freedb.org/freedb/rock/9608bd0b
+const int SIZE = 12;
+int expected[SIZE] = {2, 203, 374, 589, 818, 1070, 1326, 1522, 1692, 1744, 1922,
+                      2252};
+
+const int CDDB_ID = 0x8a08ca0b;  // No idea if this is right.
+
+// For a real test, I need to use a CD with a known CDDB_ID.
+}
+
 TEST(AudioCDReaderHelper, getTrackOffsets) {
   Array<int> offsets;
   EXPECT_STREQ(NULL, getTrackOffsets(File("../../../data"), &offsets));
 
-  static const int SIZE = 8;
-  int expected[SIZE] = {150, 2029, 7579, 9854, 26741, 27191, 33533, 35945};
   EXPECT_EQ(offsets.size(), SIZE);
 
   for (int i = 0; i < SIZE; ++i)
     EXPECT_EQ(offsets[i], expected[i]);
+}
+
+TEST(AudioCDReaderHelper, CDDBIdComputation) {
+  int id;
+  EXPECT_STREQ(NULL, getCDDBId(File("../../../data"), &id));
+  EXPECT_EQ(id, CDDB_ID);
+  std::cerr << std::hex << id << "\n" << std::dec;
+}
+
+TEST(AudioCDReaderHelper, CDDBIdEndToEnd) {
+  EXPECT_EQ(getCDDBId(Array<int>(expected, SIZE)), CDDB_ID);
 }
 
 #endif
