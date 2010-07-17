27d0e85ffface51e8c16eab8c1103dbe29ae942d
Tom Swirly <tom@swirly.com>
2010-07-17 13:54:39 -0400

    a little more testing of AudioCDReader

--- src/rec/audio/cd/juce_AudioCDReader_test.cpp
+++ src/rec/audio/cd/juce_AudioCDReader_test.cpp
@@ -6,8 +6,7 @@
 #include "juce_amalgamated.h"
 
 #include "rec/base/scoped_ptr.h"
-
-// #include "native/mac/juce_mac_AudioCDReader_helpers.h"
+#include "native/mac/juce_mac_AudioCDReader_helpers.h"
 
 #ifdef __JUCE_MAC_AUDIOCDREADER_HELPERS_JUCEHEADER__
 
@@ -30,35 +29,55 @@ TEST(AudioCDReaderHelper, getIntValueForKey) {
 
 namespace {
 
-// http://www.freedb.org/freedb/rock/9608bd0b
-const int SIZE = 12;
-int expected[SIZE] = {2, 203, 374, 589, 818, 1070, 1326, 1522, 1692, 1744, 1922,
-                      2252};
+// This CD is Kate Bush's "Never Forever".  Interestingly enough, there are
+// half-a-dozen versions of this album, only different by a few seconds on one
+// track or another.
+//
+// http://www.freedb.org/freedb/rock/9608bd0b is the specific CD used here.
 
-const int CDDB_ID = 0x8a08ca0b;  // No idea if this is right.
+const int TRACK_COUNT = 11;
 
-// For a real test, I need to use a CD with a known CDDB_ID.
+// The offset of each track from the start, in frames.
+int TRACK_LENGTHS[TRACK_COUNT + 1] = {
+    183,
+    15240,
+    28113,
+    44215,
+    61385,
+    80298,
+    99478,
+    114173,
+    126925,
+    130820,
+    144218,
+    168953
+};
+
+const int CDDB_ID = /* 0x9608bd0b; //*/ 0x8a08ca0b;
+
 }
 
 TEST(AudioCDReaderHelper, getTrackOffsets) {
   Array<int> offsets;
   EXPECT_STREQ(NULL, getTrackOffsets(File("../../../data"), &offsets));
 
-  EXPECT_EQ(offsets.size(), SIZE);
+  EXPECT_EQ(offsets.size(), TRACK_COUNT + 1);
 
-  for (int i = 0; i < SIZE; ++i)
-    EXPECT_EQ(offsets[i], expected[i]);
+  for (int i = 0; i < TRACK_COUNT; ++i)
+    EXPECT_EQ(offsets[i], TRACK_LENGTHS[i]);
 }
 
 TEST(AudioCDReaderHelper, CDDBIdComputation) {
   int id;
   EXPECT_STREQ(NULL, getCDDBId(File("../../../data"), &id));
-  EXPECT_EQ(id, CDDB_ID);
-  std::cerr << std::hex << id << "\n" << std::dec;
+  EXPECT_EQ(CDDB_ID, id) << std::hex << CDDB_ID << ", " << id
+                         << "\n" << std::dec;
 }
 
 TEST(AudioCDReaderHelper, CDDBIdEndToEnd) {
-  EXPECT_EQ(getCDDBId(Array<int>(expected, SIZE)), CDDB_ID);
+  int id = getCDDBId(Array<int>(TRACK_LENGTHS, TRACK_COUNT + 1));
+  EXPECT_EQ(CDDB_ID, id) << std::hex << CDDB_ID << ", " << id
+                         << "\n" << std::dec;
 }
 
 #endif
