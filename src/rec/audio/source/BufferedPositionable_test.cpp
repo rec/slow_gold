b310a334767deb337def9835d11fb831d31c0f86
Tom Swirly <tom@swirly.com>
2010-08-14 21:56:07 -0400

    Tiny test improvements

--- src/rec/audio/source/BufferedPositionable_test.cpp
+++ src/rec/audio/source/BufferedPositionable_test.cpp
@@ -23,9 +23,21 @@ TEST(RecAudio, BufferedPositionable) {
   bp.setNextReadPosition(32);
   EXPECT_EQ(bp.available(), 0);
 
-  bp.setNextReadPosition(33);
+  bp.setNextReadPosition(108);
   EXPECT_EQ(bp.available(), 0);
 
+  EXPECT_TRUE(bp.fillNext(32));
+  EXPECT_EQ(bp.available(), 20);  // wrap-around...
+
+  EXPECT_TRUE(bp.fillNext(12));
+  EXPECT_EQ(bp.available(), 32);
+ 
+  AudioSampleBuffer buffer(2, 32);
+  AudioSourceChannelInfo info;
+  info.buffer = &buffer;
+  info.numSamples = 24;
+  info.startSample = 8;
+  bp.getNextAudioBlock(info);
 }
 
 }  // namespace source
