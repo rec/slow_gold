a3e96baf6bb5133eebfe94a543f0cd40468d5f31
Tom Swirly <tom@swirly.com>
2010-08-14 20:54:11 -0400

    Add script to install gcc-4.5

---
+++ scripts/install-gcc-4.5.sh
@@ -0,0 +1,7 @@
+cd /tmp &&\
+ curl -O http://distfiles.macports.org/MacPorts/MacPorts-1.9.1-10.6-SnowLeopard.dmg &&
+ open MacPorts-1.9.1-10.6-SnowLeopard.dmg &&\
+ read -p "Press any key when you have installed MacPorts" &&\
+ /opt/local/bin/port install gcc_select &&\
+ /opt/local/bin/port install gcc45
+
