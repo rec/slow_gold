8ca8b7698e48582dcc1a4d59d825d62cc29eb919
Tom Swirly <tom@swirly.com>
2010-09-07 19:26:22 -0400

    Open recent submenu in File menu.

--- src/rec/app/RecContainer.h
+++ src/rec/app/RecContainer.h
@@ -3,6 +3,7 @@
 
 #include "rec/app/ComponentContainer.h"
 #include "rec/components/MainPageComponent.h"
+#include "rec/slow/Preferences.h"
 
 namespace rec {
 
@@ -16,6 +17,22 @@ class RecContainer : public ComponentContainer<RecWindow> {
     static const char* const names[] = {"File", "Edit", NULL};
     return StringArray(names);
   }
+
+  virtual const PopupMenu getMenuForIndex(int menuIndex, const String& menuName) {
+    PopupMenu menu;
+    if (menuName == "File") {
+      rec::slow::Preferences prefs(rec::slow::getPreferences());
+      PopupMenu submenu;
+
+      int size = prefs.recent_files_size();
+      for (int i = 1; i <= size; ++i)
+        submenu.addItem(i, rec::persist::copy(prefs.recent_files(size - i)));
+      menu.addSubMenu("Open recent", submenu);
+    }
+    return menu;
+  }
+
+
 
   juce_UseDebuggingNewOperator
 };
