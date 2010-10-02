97b4cf164fc0888ab2372beceffab2803cbeaba3
Tom Swirly <tom@swirly.com>
2010-10-02 19:57:11 -0400

    Now we can read and write YAML.

--- src/rec/yaml/ReadYaml.cpp
+++ src/rec/yaml/ReadYaml.cpp
@@ -11,40 +11,35 @@ namespace {
 
 void operator>>(const YAML::Node& node, Message* to);
 
-template <typename Type>
-Type get(const YAML::Node& node) {
-  Type t;
-  node >> t;
-  return t;
+string str(const YAML::Node& node) {
+  return node.Read<string>();
 }
 
 void doRead(const YAML::Node& n, Message* m, const FieldDescriptor* f) {
   const Reflection& r = *m->GetReflection();
 
   switch (f->type()) {
-  case FD::TYPE_DOUBLE:   r.SetDouble(m, f, get<double>(n)); return;
-  case FD::TYPE_FLOAT:    r.SetFloat(m, f, get<float>(n));   return;
-  case FD::TYPE_INT64:    r.SetInt64(m, f, get<int64>(n));   return;
-  case FD::TYPE_UINT64:   r.SetUInt64(m, f, get<uint64>(n)); return;
-  case FD::TYPE_INT32:    r.SetInt32(m, f, get<int32>(n));   return;
-  case FD::TYPE_FIXED64:  r.SetUInt64(m, f, get<uint64>(n)); return;
-  case FD::TYPE_FIXED32:  r.SetUInt32(m, f, get<uint32>(n)); return;
-  case FD::TYPE_BOOL:     r.SetBool(m, f, get<bool>(n));     return;
-  case FD::TYPE_STRING:   r.SetString(m, f, get<string>(n)); return;
-  case FD::TYPE_UINT32:   r.SetUInt32(m, f, get<uint32>(n)); return;
-  case FD::TYPE_SFIXED32: r.SetInt32(m, f, get<int32>(n));   return;
-  case FD::TYPE_SFIXED64: r.SetInt64(m, f, get<int64>(n));   return;
-  case FD::TYPE_SINT32:   r.SetInt32(m, f, get<int32>(n));   return;
-  case FD::TYPE_SINT64:   r.SetInt64(m, f, get<int64>(n));   return;
-  case FD::TYPE_GROUP:    n >> r.MutableMessage(m, f);       return;
-  case FD::TYPE_MESSAGE:  n >> r.MutableMessage(m, f);       return;
-  case FD::TYPE_BYTES:    r.SetString(m, f, get<string>(n)); return;
+  case FD::TYPE_DOUBLE:   r.SetDouble(m, f, double(n)); break;
+  case FD::TYPE_FLOAT:    r.SetFloat(m, f, float(n));   break;
+  case FD::TYPE_INT64:    r.SetInt64(m, f, int64(n));   break;
+  case FD::TYPE_UINT64:   r.SetUInt64(m, f, uint64(n)); break;
+  case FD::TYPE_INT32:    r.SetInt32(m, f, int32(n));   break;
+  case FD::TYPE_FIXED64:  r.SetUInt64(m, f, uint64(n)); break;
+  case FD::TYPE_FIXED32:  r.SetUInt32(m, f, uint32(n)); break;
+  case FD::TYPE_BOOL:     r.SetBool(m, f, bool(n));     break;
+  case FD::TYPE_STRING:   r.SetString(m, f, str(n));    break;
+  case FD::TYPE_UINT32:   r.SetUInt32(m, f, uint32(n)); break;
+  case FD::TYPE_SFIXED32: r.SetInt32(m, f, int32(n));   break;
+  case FD::TYPE_SFIXED64: r.SetInt64(m, f, int64(n));   break;
+  case FD::TYPE_SINT32:   r.SetInt32(m, f, int32(n));   break;
+  case FD::TYPE_SINT64:   r.SetInt64(m, f, int64(n));   break;
+  case FD::TYPE_GROUP:    n >> r.MutableMessage(m, f);  break;
+  case FD::TYPE_MESSAGE:  n >> r.MutableMessage(m, f);  break;
+  case FD::TYPE_BYTES:    r.SetString(m, f, str(n));    break;
   case FD::TYPE_ENUM:
-    r.SetEnum(m, f, f->enum_type()->FindValueByName(get<string>(n)));
-    return;
-  default: LOG(ERROR) << "Didn't understand type " << f->type(); return;
-  }
-
+    r.SetEnum(m, f, f->enum_type()->FindValueByName(str(n)));
+    break;
+  default: LOG(ERROR) << "Didn't understand type " << f->type(); break; }
 }
 
 void doReadRepeated(const YAML::Node& node, Message* m, const FieldDescriptor* f) {
@@ -53,42 +48,25 @@ void doReadRepeated(const YAML::Node& node, Message* m, const FieldDescriptor* f
   for (int i = 0; i < node.size(); ++i) {
     const YAML::Node& n = node[i];
     switch (f->type()) {
-    case FD::TYPE_DOUBLE:
-      r.AddDouble(m, f, get<double>(n)); break;
-    case FD::TYPE_FLOAT:
-      r.AddFloat(m, f, get<float>(n));   break;
-    case FD::TYPE_INT64:
-      r.AddInt64(m, f, get<int64>(n));   break;
-    case FD::TYPE_UINT64:
-      r.AddUInt64(m, f, get<uint64>(n)); break;
-    case FD::TYPE_INT32:
-      r.AddInt32(m, f, get<int32>(n));   break;
-    case FD::TYPE_FIXED64:
-      r.AddUInt64(m, f, get<uint64>(n)); break;
-    case FD::TYPE_FIXED32:
-      r.AddUInt32(m, f, get<uint32>(n)); break;
-    case FD::TYPE_BOOL:
-      r.AddBool(m, f, get<bool>(n));     break;
-    case FD::TYPE_STRING:
-      r.AddString(m, f, get<string>(n)); break;
-    case FD::TYPE_UINT32:
-      r.AddUInt32(m, f, get<uint32>(n)); break;
-    case FD::TYPE_SFIXED32:
-      r.AddInt32(m, f, get<int32>(n));   break;
-    case FD::TYPE_SFIXED64:
-      r.AddInt64(m, f, get<int64>(n));   break;
-    case FD::TYPE_SINT32:
-      r.AddInt32(m, f, get<int32>(n));   break;
-    case FD::TYPE_SINT64:
-      r.AddInt64(m, f, get<int64>(n));   break;
-    case FD::TYPE_GROUP:
-      n >> r.AddMessage(m, f);       break;
-    case FD::TYPE_MESSAGE:
-      n >> r.AddMessage(m, f);       break;
-    case FD::TYPE_BYTES:
-      r.AddString(m, f, get<string>(n)); break;
+    case FD::TYPE_DOUBLE: r.AddDouble(m, f, double(n));  break;
+    case FD::TYPE_FLOAT: r.AddFloat(m, f, float(n));     break;
+    case FD::TYPE_INT64: r.AddInt64(m, f, int64(n));     break;
+    case FD::TYPE_UINT64: r.AddUInt64(m, f, uint64(n));  break;
+    case FD::TYPE_INT32: r.AddInt32(m, f, int32(n));     break;
+    case FD::TYPE_FIXED64: r.AddUInt64(m, f, uint64(n)); break;
+    case FD::TYPE_FIXED32: r.AddUInt32(m, f, uint32(n)); break;
+    case FD::TYPE_BOOL: r.AddBool(m, f, bool(n));        break;
+    case FD::TYPE_STRING: r.AddString(m, f, str(n));     break;
+    case FD::TYPE_UINT32: r.AddUInt64(m, f, uint32(n));  break;
+    case FD::TYPE_SFIXED32: r.AddInt32(m, f, int32(n));  break;
+    case FD::TYPE_SFIXED64: r.AddInt64(m, f, int64(n));  break;
+    case FD::TYPE_SINT32: r.AddInt32(m, f, int32(n));    break;
+    case FD::TYPE_SINT64: r.AddInt64(m, f, int64(n));    break;
+    case FD::TYPE_GROUP: n >> r.AddMessage(m, f);        break;
+    case FD::TYPE_MESSAGE: n >> r.AddMessage(m, f);      break;
+    case FD::TYPE_BYTES: r.AddString(m, f, str(n));      break;
     case FD::TYPE_ENUM:
-      r.AddEnum(m, f, f->enum_type()->FindValueByName(get<string>(n)));
+      r.AddEnum(m, f, f->enum_type()->FindValueByName(str(n)));
       break;
     default:
       LOG(ERROR) << "Didn't understand type " << f->type(); break;
@@ -98,7 +76,7 @@ void doReadRepeated(const YAML::Node& node, Message* m, const FieldDescriptor* f
 
 void operator>>(const YAML::Node& node, Message* to) {
   for (YAML::Iterator i = node.begin(); i != node.end(); ++i) {
-    string name = get<string>(i.first());
+    string name = str(i.first());
     if (const FieldDescriptor* f = to->GetDescriptor()->FindFieldByName(name)) {
       if (f->label() == FieldDescriptor::LABEL_REPEATED)
         doReadRepeated(i.second(), to, f);
