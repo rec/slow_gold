417144fc19443927975b3c807fe07ab744cfcef0
Tom Swirly <tom@swirly.com>
2010-10-13 16:30:42 -0400

    Fix protos.sh

--- scripts/protos.sh
+++ scripts/protos.sh
@@ -16,6 +16,6 @@ find "$PROTO_ROOT" -name \*.pb.\* | xargs rm
 find "$SRC_ROOT" -name \*.proto |\
  xargs "$INSTALL_DIR/bin/protoc"\
    -I"$SRC_ROOT"\
-   -I"$ROOT/build/mac/$CONFIG/protobuf/include"\
+   -I"$ROOT/build/$CONFIG/protobuf/include"\
    --cpp_out="$PROTO_ROOT"
 
