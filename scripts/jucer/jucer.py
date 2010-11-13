b8c41bb9a3952644decbd18d67786e1e412c78dd
Tom Swirly <tom@swirly.com>
2010-11-12 22:42:30 -0500

    Fixed unstable ids for jucer files

--- scripts/jucer/jucer.py
+++ scripts/jucer/jucer.py
@@ -15,16 +15,13 @@ import package
 
 
 class Jucer(dom_file.DomFile):
-  CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  ID_SIZE = 9
-
-
   def __init__(self, filename, is_test, libraries, root, platform='mac'):
     dom_file.DomFile.__init__(self, filename)
     self.is_test = is_test
     self.libraries = libraries
     self.root = root
     self.platform = platform
+    self.file_id_dict = {}
 
 
   def modify(self):
@@ -60,47 +57,71 @@ class Jucer(dom_file.DomFile):
                      'DONT_SET_USING_JUCE_NAMESPACE JUCE_DONT_DEFINE_MACRO')
 
 
+  def setFileIdDict(self, n, path, depth=0):
+    if path:
+      path += '/'
+    path += n.getAttribute('name')
+
+    self.file_id_dict[path] = n.getAttribute('id')
+    for g in n.childNodes:
+      if hasattr(g, 'tagName') and g.tagName in ['GROUP', 'FILE']:
+        self.setFileIdDict(g, path, depth + 1)
+
+
+  def createFromDict(self, xmlName, path, **attributes):
+    id = self.file_id_dict.get(path + '/' + attributes['name'], None)
+    if id:
+      attributes.update(id=id)
+    return self.create(xmlName, **attributes)
+
+
   def setMaingroup(self):
     old = self.element('MAINGROUP')
-    maingroup = self.create('MAINGROUP', name=old.getAttribute('name'))
+    name = old.getAttribute('name')
+    self.setFileIdDict(old, '')
+    #  print self.file_id_dict
+
+    maingroup = self.createFromDict('MAINGROUP', name, name=name)
     self.documentElement.replaceChild(maingroup, old)
 
-    for prefix, name in (('src', 'rec'),
-                         ('genfiles', 'proto')):
-      self.createCPPFileGroup(maingroup, prefix, name, '%s/rec' % self.root)
-    maingroup.appendChild(self.createFile('Main.cpp', 'Main.cpp'))
+    for prefix, n2 in (('src', 'rec'), ('genfiles', 'proto')):
+      self.createCPPFileGroup(maingroup, prefix, n2, '%s/rec' % self.root, name)
+    maingroup.appendChild(self.createFile('Main.cpp', 'Main.cpp',
+                                          name + '/Main.cpp'))
 
 
   def join(self, files, joiner=' '):
     return joiner.join(filter(self.accept, files))
 
 
-  def createCPPFileGroup(self, parent, prefix, name, root):
+  def createCPPFileGroup(self, parent, prefix, name, root, path):
     tree = filetree.filetree('%s/%s/%s' % (root, prefix, name), self.acceptCpp)
-    parent.appendChild(self.createFileOrGroup(prefix, name, tree))
+    parent.appendChild(self.createFileOrGroup(prefix, name, tree, path))
 
 
-  def createFile(self, name, file):
-    return self.create('FILE',
-                       name=name,
-                       resource='0',
-                       file=file,
-                       compile=str(int(not file.endswith('.h'))))
+  def createFile(self, name, file, path):
+    return self.createFromDict('FILE',
+                               path,
+                               name=name,
+                               resource='0',
+                               file=file,
+                               compile=str(int(not file.endswith('.h'))))
 
 
-  def createFileOrGroup(self, prefix, name, tree):
+  def createFileOrGroup(self, prefix, name, tree, path):
     if type(tree) is str:
-      return self.createFile(name, '../../%s/%s' % (prefix, name))
+      return self.createFile(name, '../../%s/%s' % (prefix, name), path)
 
     else:
-      group = self.create('GROUP', name=name)
+      group = self.createFromDict('GROUP', path, name=name)
       if prefix:
         prefix = '%s/%s' % (prefix, name)
       else:
         prefix = name
 
+      new_path = path + '/' + name
       for k, v in tree.iteritems():
-        group.appendChild(self.createFileOrGroup(prefix, k, v))
+        group.appendChild(self.createFileOrGroup(prefix, k, v, new_path))
 
       return group
 
