14fa95b5bebb0aea5878e89a36d1d70c76600794
Tom Swirly <tom@swirly.com>
2010-12-05 13:28:55 -0500

    Lots of new stuff plus start of day Sunday

--- src/rec/audio/source/StretchyFactory.cpp
+++ src/rec/audio/source/StretchyFactory.cpp
@@ -25,6 +25,52 @@ Runny* filledRunny(const Track& track, Thread* thread) {
   return NULL;
 }
 
+
+typedef rec::util::thread::Factory<Runny, const Track&> StretchyFactoryBase;
+
+class StretchyFactory : public StretchyFactoryBase {
+ public:
+  StretchyFactory() {}
+
+  virtual Runny* makeProduct(const Track& track) {
+    if (threadShouldExit())
+      return NULL;
+
+    AudioFormatReader* reader = widget::tree::createReader(track.file());
+    Source* source = new juce::AudioFormatReaderSource(reader, true);
+    Stretch* stretchy = new Stretchy(source);
+    stretchy->setDescription(track.timestretch());
+
+    scoped_ptr<Runny> runny(new Runny(track.runny(), stretchy));
+    do {
+      if (threadShouldExit())
+        return NULL;
+    } while (!runny->fill());
+
+    return runny.transfer();
+  };
+
+ private:
+  DISALLOW_COPY_ASSIGN_AND_EMPTY(StretchyFactory);
+};
+
+
+#if 0
+    trash_.empty();
+
+  util::thread::Trash trash_;
+
+
+  void signalThreadShouldExit() {
+    Thread::signalThreadShouldExit();
+    trash_.signalThreadShouldExit();
+  }
+
+
+  ChangeLocker<Description> changeLocker_;
+
+#endif
+
 }  // namespace source
 }  // namespace audio
 }  // namespace rec
