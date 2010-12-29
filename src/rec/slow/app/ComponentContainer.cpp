0f74ac38193afe930b7c2992176cdfeab2b8f928
Tom Swirly <tom@swirly.com>
2010-12-28 19:06:53 -0500

    Split ComponentContainer.

---
+++ src/rec/slow/app/ComponentContainer.cpp
@@ -0,0 +1,86 @@
+#include "rec/slow/app/ComponentContainer.h"
+
+#include "rec/gui/RecentFiles.h"
+#include "rec/util/file/VolumeFile.h"
+#include "rec/slow/app/MainPageComponent.h"
+#include "rec/util/thread/MakeThread.h"
+
+namespace rec {
+namespace slow {
+
+ComponentContainer::ComponentContainer(MainPageComponent* c) : mainPage_(c) {
+  addAndMakeVisible(c);
+}
+
+ComponentContainer::~ComponentContainer() {}
+
+void ComponentContainer::resized() {
+  mainPage_->setBounds(0, 0, getWidth(), getHeight());
+}
+
+const StringArray ComponentContainer::getMenuBarNames() {
+  static const char* const names[] = {"File", "Edit", NULL};
+  return StringArray(names);
+}
+
+const PopupMenu ComponentContainer::getMenuForIndex(int menuIndex, const String& menuName) {
+  DLOG(INFO) << "getMenuForIndex";
+  PopupMenu menu;
+  if (menuName == "File") {
+    menu.addItem(OPEN, "Open...");
+    menu.addItem(CLOSE, "Close");
+
+    gui::RecentFiles recent = gui::getSortedRecentFiles();
+    PopupMenu submenu;
+    for (int i = 0; i < recent.file_size(); ++i)
+      submenu.addItem(RECENT_FILES + i, getFilename(recent.file(i).file()));
+
+    menu.addSubMenu("Open recent", submenu);
+    menu.addItem(EJECT, "Eject all discs");
+    menu.addItem(QUIT, "Quit");
+
+  } else if (menuName == "Edit") {
+    menu.addItem(CUT, "Cut");
+    menu.addItem(PASTE, "Paste");
+  }
+
+  return menu;
+}
+
+void ComponentContainer::quit() {
+  JUCEApplication::getInstance()->systemRequestedQuit();
+}
+
+void ComponentContainer::eject() {
+  StringArray burners = AudioCDBurner::findAvailableDevices();
+  for (int i = 0; i < burners.size(); ++i)
+    ptr<AudioCDBurner>(AudioCDBurner::openDevice(i))->openTray();
+}
+
+void ComponentContainer::menuItemSelected(int menuItemID, int topLevelMenuIndex) {
+  thread::runInNewThread("doMenuSelected", 5,
+                         this, &ComponentContainer::doMenuItemSelected,
+                         menuItemID, topLevelMenuIndex);
+}
+
+void ComponentContainer::doMenuItemSelected(int menuItemID, int topLevelMenuIndex) {
+  DLOG(INFO) << "menuItemSelected: "
+             << menuItemID << ", " << topLevelMenuIndex;
+  MainPage* mainPage = mainPage_->mainPage();
+  switch (menuItemID) {
+    case OPEN:   mainPage->doOpen(); break;
+    case CLOSE:  mainPage->doClose(); break;
+    case CUT:    mainPage_->cut(); break;
+    case PASTE:  mainPage_->paste(); break;
+    case EJECT:  eject(); break;
+    case QUIT:   quit(); break;
+    default:     mainPage_->loadRecentFile(menuItemID - RECENT_FILES); break;
+  }
+}
+
+ApplicationCommandTarget* ComponentContainer::getNextCommandTarget() {
+  return findFirstTargetParentComponent();
+}
+
+}  // namespace slow
+}  // namespace rec
--- src/rec/slow/app/ComponentContainer.h
+++ src/rec/slow/app/ComponentContainer.h
@@ -2,106 +2,39 @@
 #define __REC_COMPONENT_CONTAINER
 
 #include "rec/base/base.h"
-#include "rec/gui/RecentFiles.h"
-#include "rec/util/file/VolumeFile.h"
-#include "rec/slow/app/MainPageComponent.h"
-#include "rec/util/thread/MakeThread.h"
 
 using namespace juce;
 
 namespace rec {
 namespace slow {
 
+class MainPageComponent;
+
 class ComponentContainer : public Component,
                            public MenuBarModel,
                            public ApplicationCommandTarget {
  public:
-  ComponentContainer(MainPageComponent* c) : mainPage_(c) { addAndMakeVisible(c); }
+  ComponentContainer(MainPageComponent* c);
+  virtual ~ComponentContainer();
 
-  virtual ~ComponentContainer() {}
+  virtual void resized();
+  virtual const StringArray getMenuBarNames();
 
-  virtual void resized() {
-    mainPage_->setBounds(0, 0, getWidth(), getHeight());
-  }
+  enum MenuItems { OPEN = 1, CLOSE, CUT, PASTE, QUIT, EJECT, RECENT_FILES };
 
-  virtual const StringArray getMenuBarNames() {
-    static const char* const names[] = {"File", "Edit", NULL};
-    return StringArray(names);
-  }
+  virtual const PopupMenu getMenuForIndex(int menuIndex, const String& menuName);
 
-  enum MenuItems {
-    OPEN = 1,
-    CLOSE,
-    CUT,
-    PASTE,
-    QUIT,
-    EJECT,
-    RECENT_FILES
-  };
-
-  virtual const PopupMenu getMenuForIndex(int menuIndex, const String& menuName) {
-    DLOG(INFO) << "getMenuForIndex";
-    PopupMenu menu;
-    if (menuName == "File") {
-      menu.addItem(OPEN, "Open...");
-      menu.addItem(CLOSE, "Close");
-
-      gui::RecentFiles recent = gui::getSortedRecentFiles();
-      PopupMenu submenu;
-      for (int i = 0; i < recent.file_size(); ++i)
-        submenu.addItem(RECENT_FILES + i, getFilename(recent.file(i).file()));
-
-      menu.addSubMenu("Open recent", submenu);
-      menu.addItem(EJECT, "Eject all discs");
-      menu.addItem(QUIT, "Quit");
-
-    } else if (menuName == "Edit") {
-      menu.addItem(CUT, "Cut");
-      menu.addItem(PASTE, "Paste");
-    }
-
-    return menu;
-  }
-
-  void quit() {
-    JUCEApplication::getInstance()->systemRequestedQuit();
-  }
-
-  void eject() {
-    StringArray burners = AudioCDBurner::findAvailableDevices();
-    for (int i = 0; i < burners.size(); ++i)
-      ptr<AudioCDBurner>(AudioCDBurner::openDevice(i))->openTray();
-  }
-
-  virtual void menuItemSelected(int menuItemID, int topLevelMenuIndex) {
-    thread::runInNewThread("doMenuSelected", 5, 
-    											 this, &ComponentContainer::doMenuItemSelected,
-                           menuItemID, topLevelMenuIndex);
-  }
-
-  void doMenuItemSelected(int menuItemID, int topLevelMenuIndex) {
-    DLOG(INFO) << "menuItemSelected: "
-               << menuItemID << ", " << topLevelMenuIndex;
-  	MainPage* mainPage = mainPage_->mainPage();
-    switch (menuItemID) {
-      case OPEN:   mainPage->doOpen(); break;
-      case CLOSE:  mainPage->doClose(); break;
-      case CUT:    mainPage_->cut(); break;
-      case PASTE:  mainPage_->paste(); break;
-      case EJECT:  eject(); break;
-      case QUIT:   quit(); break;
-      default:     mainPage_->loadRecentFile(menuItemID - RECENT_FILES); break;
-    }
-  }
+  void quit();
+  void eject();
+  virtual void menuItemSelected(int menuItemID, int topLevelMenuIndex);
+  void doMenuItemSelected(int menuItemID, int topLevelMenuIndex);
 
   // ApplicationCommandTarget virtual methods.
   virtual void getAllCommands(Array <CommandID>& commands) {}
   virtual void getCommandInfo(CommandID commandID, ApplicationCommandInfo& result) {}
   virtual bool perform(const InvocationInfo& info) { return false; }
 
-  virtual ApplicationCommandTarget* getNextCommandTarget() {
-    return findFirstTargetParentComponent();
-  }
+  virtual ApplicationCommandTarget* getNextCommandTarget();
 
  protected:
   ptr<MainPageComponent> mainPage_;
