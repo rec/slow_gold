5039441e45c29c68c11fd4ac36caca2ec3c919c9
Tom Swirly <tom@swirly.com>
2011-03-05 21:12:48 +0800

    Runny_test incremental changes

--- src/rec/audio/source/Runny_test.cpp
+++ src/rec/audio/source/Runny_test.cpp
@@ -7,31 +7,48 @@ namespace rec {
 namespace audio {
 namespace source {
 
-TEST(RecAudio, Runny) {
-  static const int READ_OFFSET = 16;
-  static const int WRITE_OFFSET = 64;
+namespace {
 
-  AudioSampleBuffer buffer(2, 128);
+const int READ_OFFSET = 16;
+const int WRITE_OFFSET = 64;
+const int NUM_SAMPLES = 32;
+const int BUFFER_SIZE = 128;
+const int CHUNK_SIZE = 16;
+const int CHANNELS = 2;
+
+void runTest(int numSamples) {
+  AudioSampleBuffer buffer(CHANNELS, BUFFER_SIZE);
 
   AudioSourceChannelInfo info;
   info.buffer = &buffer;
-  info.numSamples = 32;
+  info.numSamples = numSamples;
   info.startSample = WRITE_OFFSET;
 
   RunnyProto runnyProto;
-  // runnyProto.set_chunk_size(16);
+  runnyProto.set_chunk_size(CHUNK_SIZE);
   Runny runny(new Testy, runnyProto);
+  runny.fillOnce();
   runny.fillOnce();
 
   runny.setNextReadPosition(READ_OFFSET);
   runny.getNextAudioBlock(info);
 
-  for (int c = 0; c < 2; ++c) {
-    for (int i = 0; i < 32; ++i) {
+  for (int c = 0; c < CHANNELS; ++c) {
+    for (int i = 0; i < numSamples; ++i) {
       EXPECT_EQ(Testy::getSample(READ_OFFSET + i),
-                *buffer.getSampleData(c, WRITE_OFFSET + i));
+                *buffer.getSampleData(c, WRITE_OFFSET + i))
+        << "channel: " << c
+        << ", sample: " << i
+        << ", numSamples: " << numSamples
+;
     }
   }
+}
+
+}
+
+TEST(RecAudio, Runny) {
+  runTest(NUM_SAMPLES);
 }
 
 }  // namespace source
