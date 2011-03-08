3776949459df7cf1d05151d15cd110f6b80e12fb
Tom Swirly <tom@swirly.com>
2011-03-08 19:09:11 +0800

    Tiny changes to make app build again

--- src/rec/audio/source/Stretchy.cpp
+++ src/rec/audio/source/Stretchy.cpp
@@ -28,7 +28,7 @@ int64 Stretchy::getNextReadPosition() const {
 }
 
 void Stretchy::setNextReadPosition(int64 position) {
-  source()->setNextReadPosition(position / timeScale(description_));1
+  source()->setNextReadPosition(position / timeScale(description_));
 }
 
 void Stretchy::getNextAudioBlock(const AudioSourceChannelInfo& info) {
