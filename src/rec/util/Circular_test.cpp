3ea639f98173085603907ae1a1f264a5eec127fe
Tom Swirly <tom@swirly.com>
2011-04-09 21:17:28 -0400

    improved tests

--- src/rec/util/Circular_test.cpp
+++ src/rec/util/Circular_test.cpp
@@ -24,6 +24,24 @@ TEST(Circular, Fill) {
   EXPECT_TRUE(c.fillable() == Range<int>(4, 8));
 }
 
+TEST(Circular, Consume) {
+  Circular<int> c(0, 4, 8);
+  c.consume(3);
+  EXPECT_TRUE(c == Circular<int>(3, 4, 8));
+  EXPECT_TRUE(c.fillable() == Range<int>(4, 8))
+    << c.fillable().begin_ << ", " << c.fillable().end_;
+}
+
+TEST(Circular, FillAround) {
+  Circular<int> c(3, 4, 8);
+  c.fill(6);
+  EXPECT_TRUE(c == Circular<int>(3, 10, 8)) <<
+    c.begin_ << "," << c.end_ << "," << c.capacity_;
+
+  EXPECT_TRUE(c.fillable() == Range<int>(2, 3))
+    << c.fillable().begin_ << ", " << c.fillable().end_;
+}
+
 }  // namespace util
 }  // namespace rec
 
