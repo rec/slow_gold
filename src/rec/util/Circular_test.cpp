478f94620662547cd742b574ec88f6df00139995
Tom Swirly <tom@swirly.com>
2011-06-21 17:01:53 -0400

    A little more testing of Circular

--- src/rec/util/Circular_test.cpp
+++ src/rec/util/Circular_test.cpp
@@ -25,30 +25,53 @@ TEST(Circular, Fillable3) {
 
 TEST(Circular, Fill) {
   Circular<int> c(8);
+  EXPECT_EQ(c.size(), 0);
+
   c.fill(3);
+  EXPECT_EQ(c.size(), 3);
   EXPECT_TRUE(c.fillableBlock() == Range<int>(3, 8));
 
   c.fill(3);
+  EXPECT_EQ(c.size(), 6);
   EXPECT_TRUE(c.fillableBlock() == Range<int>(6, 8));
 
   c.fill(3);
+  EXPECT_EQ(c.size(), 8);
   EXPECT_TRUE(c.fillableBlock() == Range<int>(0, 0));
 
   c.fill(3);
+  EXPECT_EQ(c.size(), 8);
   EXPECT_TRUE(c.fillableBlock() == Range<int>(0, 0));
 
   c.consume(2);
+  EXPECT_EQ(c.size(), 6);
   EXPECT_TRUE(c.fillableBlock() == Range<int>(0, 2));
 
   c.consume(3);
+  EXPECT_EQ(c.size(), 3);
   EXPECT_TRUE(c.fillableBlock() == Range<int>(0, 5));
 
   c.fill(2);
+  EXPECT_EQ(c.size(), 5);
   EXPECT_TRUE(c.fillableBlock() == Range<int>(2, 5));
+
+  c.consume(2);
+  EXPECT_EQ(c.size(), 3);
+  EXPECT_TRUE(c.fillableBlock() == Range<int>(2, 7));
+
+  c.consume(2);
+  EXPECT_EQ(c.size(), 1);
+  EXPECT_TRUE(c.fillableBlock() == Range<int>(2, 8));
+
+  c.fill(2);
+  EXPECT_EQ(c.size(), 3);
+  EXPECT_TRUE(c.fillableBlock() == Range<int>(4, 8));
+
+  c.consume(2);
+  EXPECT_EQ(c.size(), 1);
+  EXPECT_TRUE(c.fillableBlock() == Range<int>(4, 8));
 }
 
 }  // namespace
 }  // namespace util
 }  // namespace rec
-
-
