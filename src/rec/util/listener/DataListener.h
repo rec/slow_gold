927962ba2ff050823e9c5d4eaf1b09d85eac13ed
Tom Swirly <tom@swirly.com>
2011-07-24 14:41:22 -0400

    Tiny clean to DataListener

--- src/rec/util/listener/DataListener.h
+++ src/rec/util/listener/DataListener.h
@@ -17,45 +17,18 @@ template <typename Proto>
 class DataListener : public Listener<const Proto&>,
                      public Listener<const VirtualFile&> {
  public:
-  DataListener() : data_(NULL) {
-    persist::setter<VirtualFile>()->addListener(this);
-  }
+  DataListener();
 
   virtual ~DataListener() { setData(NULL); }
-
   virtual void operator()(const VirtualFile& f) { setFile(f); }
-  virtual void setFile(const VirtualFile& file) {
-    LOG(INFO) << "setFile " << Proto::default_instance().GetTypeName();
-    setData(persist::setter<Proto>(file));
-  }
+  virtual void setFile(const VirtualFile& file);
 
-  virtual void operator()(const Proto& p) {
-    {
-      ScopedLock l(lock_);
-      proto_ = p;
-    }
-    onDataChange(p);
-  }
+  virtual void operator()(const Proto& p);
 
-  const Proto get() const {
-    ScopedLock l(lock_);
-    return proto_;
-  }
+  const Proto get() const;
 
  protected:
-  virtual void setData(persist::Data<Proto>* d) {
-    if (data_ != d) {
-      if (data_)
-        data_->removeListener(this);
-
-      data_ = d;
-
-      if (data_) {
-        data_->addListener(this);
-        (*this)(data_->get());
-      }
-    }
-  }
+  virtual void setData(persist::Data<Proto>* d);
 
  protected:
   virtual void onDataChange(const Proto&) = 0;
@@ -67,6 +40,48 @@ class DataListener : public Listener<const Proto&>,
  private:
   DISALLOW_COPY_AND_ASSIGN(DataListener);
 };
+
+template <typename Proto>
+DataListener<Proto>::DataListener() : data_(NULL) {
+  persist::setter<VirtualFile>()->addListener(this);
+}
+
+template <typename Proto>
+void DataListener<Proto>::setFile(const VirtualFile& file) {
+  LOG(INFO) << "setFile " << Proto::default_instance().GetTypeName();
+  setData(persist::setter<Proto>(file));
+}
+
+template <typename Proto>
+void DataListener<Proto>::operator()(const Proto& p) {
+  {
+    ScopedLock l(lock_);
+    proto_ = p;
+  }
+  onDataChange(p);
+}
+
+template <typename Proto>
+const Proto DataListener<Proto>::get() const {
+  ScopedLock l(lock_);
+  return proto_;
+}
+
+template <typename Proto>
+void DataListener<Proto>::setData(persist::Data<Proto>* d) {
+  if (data_ != d) {
+    if (data_)
+      data_->removeListener(this);
+
+    data_ = d;
+
+    if (data_) {
+      data_->addListener(this);
+      (*this)(data_->get());
+    }
+  }
+}
+
 
 }  // namespace listener
 }  // namespace util
