cd2537a43406fd90680dc4a405ad856f460bba1d
Tom Swirly <tom@swirly.com>
2011-10-17 17:25:25 -0400

    The problem continues

--- scripts/makeAllFiles.sh
+++ scripts/makeAllFiles.sh
@@ -1,7 +1,6 @@
 
 (\
  cd $ROOT/rec &&\
- make -s &&\
  "$ROOT/rec/scripts/makeProtoFiles.sh" &&\
  "$ROOT/rec/scripts/makeProjectFiles.sh" &&\
  "$ROOT/rec/scripts/makeEmacsTags.sh"\
--- scripts/runJucer.sh
+++ scripts/runJucer.sh
@@ -1,4 +1,4 @@
 NAME=$1
 
-"/Users/tom/Documents/development/juce/extras/the jucer/build/mac/build/Debug/Jucer.app" --resave "$ROOT/rec/projects/$NAME/$NAME.jucer" > "/tmp/$NAME.jucer.log" && \
+"/development/juce/extras/Introjucer/Builds/MacOSX/build/Debug/Introjucer.app/Contents/MacOS/IntroJucer" --resave "$ROOT/rec/projects/$NAME/$NAME.jucer" > "/tmp/$NAME.jucer.log" && \
  touch -t 201101010000 "$ROOT/rec/projects/$NAME/JuceLibraryCode/"Juce*
