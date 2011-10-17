7d01f124b784074c70414f00d0847e650bc38593
Tom Swirly <tom@swirly.com>
2011-10-17 15:33:40 -0400

    new juce, still have link issue

--- scripts/runJucer.sh
+++ scripts/runJucer.sh
@@ -1,4 +1,4 @@
 NAME=$1
 
-"$JUCER" --resave "$ROOT/rec/projects/$NAME/$NAME.jucer" > "/tmp/$NAME.jucer.log" && \
+Jucer --resave "$ROOT/rec/projects/$NAME/$NAME.jucer" > "/tmp/$NAME.jucer.log" && \
  touch -t 201101010000 "$ROOT/rec/projects/$NAME/JuceLibraryCode/"Juce*
