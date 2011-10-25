2e901e2282e9ec1918b17af8c1fb2b121d825ef0
Tom Swirly <tom@swirly.com>
2011-10-25 12:42:15 -0400

    Even, even more simplification of Listeners

--- src/rec/slow/Listeners.h
+++ src/rec/slow/Listeners.h
@@ -1,10 +1,7 @@
 #ifndef __REC_SLOW_LISTENERS__
 #define __REC_SLOW_LISTENERS__
 
-#include "rec/audio/Audio.h"
-#include "rec/command/Command.pb.h"
 #include "rec/slow/HasInstance.h"
-#include "rec/util/Range.h"
 #include "rec/util/listener/Listener.h"
 
 namespace rec {
@@ -17,7 +14,6 @@ class Instance;
 class MouseListener;
 
 class Listeners : public Listener<const gui::DropFiles&>,
-                  public juce::MouseListener,
                   public HasInstance {
  public:
   explicit Listeners(Instance* i);
