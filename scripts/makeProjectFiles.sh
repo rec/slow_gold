cd4c7ebc00eaaaceed374c5bcb374f98b143ef5e
Paul Callahan <paul.callahan@gmail.com>
2011-10-30 12:43:36 -0700

    remove absolute path from vs2010 slow.jucer, add linker path

--- scripts/makeProjectFiles.sh
+++ scripts/makeProjectFiles.sh
@@ -2,16 +2,16 @@
 \
  ICON_SOURCE="$ROOT/rec/art/icons" &&\
  ICON_MAC="$ROOT/rec/projects/slow/Builds/MacOSX" &&\
- ICON_WINDOWS="$ROOT/rec/projects/slow/Builds/VisualStudio2008" &&\
+ ICON_WINDOWS="$ROOT/rec/projects/slow/Builds/VisualStudio2010" &&\
 \
  mkdir -p "$ICON_MAC" "$ICON_WINDOWS" &&\
 \
  cp -p "$ICON_SOURCE/SampleIconMac.icns" "$ICON_MAC/Icon.icns" &&\
  cp -p "$ICON_SOURCE/SampleIconWindows.ico" "$ICON_WINDOWS/icon.ico" &&\
 \
- runJucer.sh tests &&\
- runJucer.sh slow &&\
- runJucer.sh command &&\
+ $ROOT/rec/scripts/runJucer.sh tests &&\
+ $ROOT/rec/scripts/runJucer.sh slow &&\
+ $ROOT/rec/scripts/runJucer.sh command &&\
 \
  cp -p "$ICON_SOURCE/SampleIconMac.icns" "$ICON_MAC/Icon.icns" &&\
  cp -p "$ICON_SOURCE/SampleIconWindows.ico" "$ICON_WINDOWS/icon.ico"\
--- scripts/runJucer.sh
+++ scripts/runJucer.sh
@@ -1,4 +1,13 @@
 NAME=$1
 
-"/development/juce/extras/Introjucer/Builds/MacOSX/build/Debug/Introjucer.app/Contents/MacOS/IntroJucer" --resave "$ROOT/rec/projects/$NAME/$NAME.jucer" > "/tmp/$NAME.jucer.log" && \
+PLATFORM=`uname`
+
+if [[ $PLATFORM == CYGWIN* ]] 
+then
+    INTROJUCER_APP="/apps/juce/extras/Introjucer/Builds/VisualStudio2010/Release/IntroJucer.exe"
+else    
+    INTROJUCER_APP="/development/juce/extras/Introjucer/Builds/MacOSX/build/Debug/Introjucer.app/Contents/MacOS/IntroJucer"
+fi
+
+${INTROJUCER_APP} --resave "$ROOT/rec/projects/$NAME/$NAME.jucer" > "/tmp/$NAME.jucer.log" && \
  touch -t 201101010000 "$ROOT/rec/projects/$NAME/JuceLibraryCode/"Juce*
