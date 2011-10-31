5974c86701a87425ef7daac2706ab61bf30b95cb
Tom Swirly <tom@swirly.com>
2011-10-31 01:26:04 -0400

    Refactoring the stretchies

--- src/rec/audio/stretch/SoundTouchStretchy.cpp
+++ src/rec/audio/stretch/SoundTouchStretchy.cpp
@@ -1,7 +1,5 @@
 #include "rec/audio/stretch/SoundTouchStretchy.h"
 #include "rec/audio/stretch/Stretch.h"
-#include "rec/audio/stretch/Stretchy.h"
-#include "rec/audio/util/Frame.h"
 #include "rec/audio/soundtouch/SoundTouch.h"
 
 namespace rec {
@@ -10,89 +8,70 @@ namespace stretch {
 
 using soundtouch::SoundTouch;
 
-namespace {
+SoundTouchStretchy::SoundTouchStretchy(PositionableAudioSource* s, const Stretch& stretch)
+    : Stretchy(s, stretch), buffer_(2, INPUT_BUFFER_SAMPLES) {
+  fetchInfo_.numSamples = INPUT_BUFFER_SAMPLES;
+  fetchInfo_.startSample = 0;
+  fetchInfo_.buffer = &buffer_;
+  frames_.setLength(INPUT_BUFFER_SAMPLES);
+}
 
-class SoundTouchStretchy : public Stretchy {
- public:
-  static const int INPUT_BUFFER_SAMPLES = 4096;
+SoundTouchStretchy::~SoundTouchStretchy() {}
 
-  SoundTouchStretchy(PositionableAudioSource* s, const Stretch& stretch)
-      : Stretchy(s, stretch), buffer_(2, INPUT_BUFFER_SAMPLES) {
-    fetchInfo_.numSamples = INPUT_BUFFER_SAMPLES;
-    fetchInfo_.startSample = 0;
-    fetchInfo_.buffer = &buffer_;
-    frames_.setLength(INPUT_BUFFER_SAMPLES);
-  }
-  virtual ~SoundTouchStretchy() {}
+void SoundTouchStretchy::initializeStretcher(const Stretch& stretch) {
+  soundTouch_.reset(new SoundTouch);
+  soundTouch_->setRate(static_cast<float>(timeScale(stretch)));
+  soundTouch_->setChannels(stretch.channels());
+  soundTouch_->setPitch(pitchScale(stretch));
+}
 
-  void initializeStretcher(const Stretch& stretch) {
-    soundTouch_.reset(new SoundTouch);
-    soundTouch_->setRate(static_cast<float>(timeScale(stretch)));
-    soundTouch_->setChannels(stretch.channels());
-    soundTouch_->setPitch(pitchScale(stretch));
-  }
+void SoundTouchStretchy::nextStretchedAudioBlock(const AudioSourceChannelInfo& info) {
+  fillInput(info.numSamples);
+  copyToOutput(info);
+}
 
- protected:
-  virtual void nextStretchedAudioBlock(const AudioSourceChannelInfo& info) {
-    fillInput(info.numSamples);
-    copyToOutput(info);
-  }
-
-  bool fillInput(int numSamples) {
-    static const int MAXIMUM_FAILURE_COUNT = 10;
-    for (int fail = 0, s; (s = soundTouch_->numSamples()) && s < numSamples; ) {
-      putSamples(fetchInfo_);
-      if (s == soundTouch_->numSamples()) {
-        if (++fail > MAXIMUM_FAILURE_COUNT) {
-          LOG(ERROR) << "Made no progress in pitch shifter";
-          return false;
-        }
-      } else {
-        fail = 0;
+bool SoundTouchStretchy::fillInput(int numSamples) {
+  static const int MAXIMUM_FAILURE_COUNT = 10;
+  for (int fail = 0, s; (s = soundTouch_->numSamples()) && s < numSamples; ) {
+    putSamples(fetchInfo_);
+    if (s == soundTouch_->numSamples()) {
+      if (++fail > MAXIMUM_FAILURE_COUNT) {
+        LOG(ERROR) << "Made no progress in pitch shifter";
+        return false;
       }
-    }
-    return true;
-  }
-
-  void putSamples(const AudioSourceChannelInfo& info) {
-    Stretchy::getNextAudioBlock(info);
-    FloatFrame* frame = frames_.frames();
-    for (int i = 0; i < info.numSamples; ++i, ++frame) {
-      for (int c = 0; c < 2; ++c)
-        frame->sample_[c] = *buffer_.getSampleData(c, i);
-    }
-
-    soundTouch_->putSamples(frames_.frames()->sample_, info.numSamples);
-  }
-
-  void copyToOutput(const AudioSourceChannelInfo& info) {
-    int n = info.numSamples;
-    if (frames_.length() < n)
-      frames_.setLength(n);
-    uint rec = soundTouch_->receiveSamples(frames_.frames()->sample_, n);
-
-    if (static_cast<int>(rec) != n)
-      LOG(ERROR) << "Expected " << n << " got " << rec;
-
-    FloatFrame* frame = frames_.frames();
-    for (int i = 0; i < n; ++i, ++frame) {
-      for (int c = 0; c < 2; ++c)
-        *info.buffer->getSampleData(c, i) = frame->sample_[c];
+    } else {
+      fail = 0;
     }
   }
+  return true;
+}
 
- private:
-  Frames<FloatFrame> frames_;
+void SoundTouchStretchy::putSamples(const AudioSourceChannelInfo& info) {
+  Stretchy::getNextAudioBlock(info);
+  FloatFrame* frame = frames_.frames();
+  for (int i = 0; i < info.numSamples; ++i, ++frame) {
+    for (int c = 0; c < 2; ++c)
+      frame->sample_[c] = *buffer_.getSampleData(c, i);
+  }
 
-  ptr<SoundTouch> soundTouch_;
-  AudioSampleBuffer buffer_;
+  soundTouch_->putSamples(frames_.frames()->sample_, info.numSamples);
+}
 
-  AudioSourceChannelInfo fetchInfo_;
+void SoundTouchStretchy::copyToOutput(const AudioSourceChannelInfo& info) {
+  int n = info.numSamples;
+  if (frames_.length() < n)
+    frames_.setLength(n);
+  uint rec = soundTouch_->receiveSamples(frames_.frames()->sample_, n);
 
-  DISALLOW_COPY_ASSIGN_AND_EMPTY(SoundTouchStretchy);
-};
+  if (static_cast<int>(rec) != n)
+    LOG(ERROR) << "Expected " << n << " got " << rec;
 
-}  // namespace
+  FloatFrame* frame = frames_.frames();
+  for (int i = 0; i < n; ++i, ++frame) {
+    for (int c = 0; c < 2; ++c)
+      *info.buffer->getSampleData(c, i) = frame->sample_[c];
+  }
+}
 
 Stretchy* createSoundTouchStretchy(PositionableAudioSource* p,
                                            const Stretch& s) {
--- src/rec/audio/stretch/SoundTouchStretchy.h
+++ src/rec/audio/stretch/SoundTouchStretchy.h
@@ -2,10 +2,41 @@
 #define __REC_AUDIO_STRETCH_SOUNDTOUCHSTRETCHY__
 
 #include "rec/audio/stretch/Stretchy.h"
+#include "rec/audio/util/Frame.h"
+
+namespace soundtouch { class SoundTouch; }
 
 namespace rec {
 namespace audio {
 namespace stretch {
+
+class SoundTouchStretchy : public Stretchy {
+ public:
+  static const int INPUT_BUFFER_SAMPLES = 4096;
+
+  SoundTouchStretchy(PositionableAudioSource* s, const Stretch& stretch);
+  virtual ~SoundTouchStretchy();
+
+  virtual void initializeStretcher(const Stretch& stretch);
+
+ protected:
+  virtual void nextStretchedAudioBlock(const AudioSourceChannelInfo& info);
+
+ private:
+  bool fillInput(int numSamples);
+  void putSamples(const AudioSourceChannelInfo& info);
+  void copyToOutput(const AudioSourceChannelInfo& info);
+
+ private:
+  Frames<FloatFrame> frames_;
+
+  ptr<soundtouch::SoundTouch> soundTouch_;
+  AudioSampleBuffer buffer_;
+
+  AudioSourceChannelInfo fetchInfo_;
+
+  DISALLOW_COPY_ASSIGN_AND_EMPTY(SoundTouchStretchy);
+};
 
 Stretchy* createSoundTouchStretchy(Source* p, const Stretch& s);
 
