07cd8fe9406c04c6843d3863c1aacb1518ff9f37
Tom Swirly <tom@swirly.com>
2011-11-05 00:04:56 -0400

    Command line application works well

--- projects/command/Main.cpp
+++ projects/command/Main.cpp
@@ -12,7 +12,7 @@ DEFINE_double(time_percent, 100.0, "Percent to speed up or slow down time");
 DEFINE_int32(semitone, 0, "Semitones to shift the result");
 DEFINE_int32(cents, 0, "cents to shift the result");
 
-static const juce::String ROOT = "/development/sound-tests/results/TakeFive";
+static const juce::File ROOT("/development/sound-tests/results/TakeFive.wav");
 
 namespace rec {
 namespace audio {
@@ -23,7 +23,7 @@ using namespace rec::audio::stretch;
 
 static Stretch makeStretch() {
   Stretch s;
-  s.set_time_percent(FLAGS_time_percent / 100.0);
+  s.set_time_percent(FLAGS_time_percent);
   s.set_semitone_shift(FLAGS_semitone);
   s.set_detune_cents(FLAGS_cents);
 
@@ -31,10 +31,10 @@ static Stretch makeStretch() {
 }
 
 
-void copyFileOnce(const String& inFile, const String& outFile) {
-  File in(inFile), out(outFile);
+void copyFileOnce(const File& in, const File& out) {
   if (out.exists())
     out.deleteFile();
+
   SourceAndWriter saw = makeSourceAndWriter(in, out);
   Stretchy stretchy(saw.first);
   ptr<AudioFormatWriter> writer(saw.second);
@@ -43,15 +43,20 @@ void copyFileOnce(const String& inFile, const String& outFile) {
   writer->writeFromAudioSource(stretchy, stretchy.getTotalLength(), 4096);
 }
 
+File outFile(const File& f) {
+  String out = f.getFileNameWithoutExtension() + "-out" + f.getFileExtension();
+  return f.getParentDirectory().getChildFile(out);
+}
+
 int main(int argc, char** argv) {
   FLAGS_logtostderr = true;
   google::ParseCommandLineFlags(&argc, &argv, true);
   if (argc == 1)
-    copyFileOnce(ROOT + ".wav", "out-" + ROOT + ".wav");
+    copyFileOnce(ROOT, outFile(ROOT));
   else if (argc == 2)
-    copyFileOnce(argv[1], "out-" + String(argv[1]));
+    copyFileOnce(File(argv[1]), outFile(File(argv[1])));
   else
-    copyFileOnce(argv[1], argv[2]);
+    copyFileOnce(File(argv[1]), File(argv[2]));
 
   return 0;
 }
