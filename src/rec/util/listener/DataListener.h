fbda69ce8b3795d2203c1984fe929f30bdb52bfc
Tom Swirly <tom@swirly.com>
2011-11-16 18:10:22 -0500

    Start to split DataListener

--- src/rec/util/listener/DataListener.h
+++ src/rec/util/listener/DataListener.h
@@ -11,18 +11,15 @@ namespace rec {
 namespace util {
 namespace listener {
 
-// DataListener listens to changes in persistent data.
-
 template <typename Proto>
-class DataListener : public Listener<const Proto&>,
-                     public Listener<const VirtualFile&> {
+class DataListenerBase : public Listener<const Proto&> {
  public:
-  DataListener(bool filterDupes = false);
-  virtual ~DataListener() { } // setData(NULL); }
+  DataListenerBase(bool fd = false) : filterDupes_(fd) {}
+  virtual ~DataListenerBase() {}
 
-  virtual void operator()(const VirtualFile& f) { setFile(f); }
   virtual void operator()(const Proto& p);
-  virtual const Proto get() const;
+  virtual const Proto get() const { Lock l(lock_); return proto_; }
+
   static const bool UPDATE_ON_MESSAGE_THREAD = !true;  // TODO: fix!
 
  protected:
@@ -37,49 +34,27 @@ class DataListener : public Listener<const Proto&>,
   data::TypedEditable<Proto>* data_;
   Proto proto_;
 
-  virtual void setFile(const VirtualFile& file);
-
   const bool filterDupes_;
 
-  DISALLOW_COPY_AND_ASSIGN(DataListener);
+  DISALLOW_COPY_AND_ASSIGN(DataListenerBase);
 };
 
-//
-// Implementation.
-//
-
 template <typename Proto>
-DataListener<Proto>::DataListener(bool f) : data_(NULL), filterDupes_(f) {
-  data::editable<VirtualFile>()->addListener(this);
-}
-
-template <typename Proto>
-void DataListener<Proto>::setFile(const VirtualFile& file) {
-  setData(data::editable<Proto>(file));
-}
-
-template <typename Proto>
-void DataListener<Proto>::operator()(const Proto& p) {
+void DataListenerBase<Proto>::operator()(const Proto& p) {
   {
-    ScopedLock l(lock_);
+    Lock l(lock_);
     if (filterDupes_ && data::equals(proto_, p))
       return;
     proto_ = p;
   }
   if (UPDATE_ON_MESSAGE_THREAD)
-    thread::callAsync(this, &DataListener<Proto>::doOnDataChange, p);
+    thread::callAsync(this, &DataListenerBase<Proto>::doOnDataChange, p);
   else
     onDataChange(p);
 }
 
 template <typename Proto>
-const Proto DataListener<Proto>::get() const {
-  ScopedLock l(lock_);
-  return proto_;
-}
-
-template <typename Proto>
-void DataListener<Proto>::setData(data::TypedEditable<Proto>* d) {
+void DataListenerBase<Proto>::setData(data::TypedEditable<Proto>* d) {
   if (data_ != d) {
     if (data_)
       data_->removeListener(this);
@@ -92,6 +67,27 @@ void DataListener<Proto>::setData(data::TypedEditable<Proto>* d) {
     }
   }
 }
+
+
+// DataListener listens to changes in persistent data.
+
+template <typename Proto>
+class DataListener : public DataListenerBase<Proto>,
+                     public Listener<const VirtualFile&> {
+ public:
+  DataListener(bool filterDupes = false) : DataListenerBase<Proto>(filterDupes) {
+    data::editable<VirtualFile>()->addListener(this);
+  }
+
+  virtual ~DataListener() {}
+
+  virtual void operator()(const VirtualFile& f) {
+    this->setData(data::editable<Proto>(f));
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(DataListener);
+};
 
 
 }  // namespace listener
