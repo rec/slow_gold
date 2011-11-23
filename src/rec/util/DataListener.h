aeb5e62366ff4f8b3c1c6ca393367dad9c4ec07e
Tom Swirly <tom@swirly.com>
2011-11-22 19:04:21 -0500

    Goodbye GlobalDataListener

--- src/rec/util/DataListener.h
+++ src/rec/util/DataListener.h
@@ -15,31 +15,34 @@ namespace util {
 namespace listener {
 
 template <typename Proto>
-class GlobalDataListener : public Listener<const Proto&>,
+class DataListener : public Listener<const Proto&>,
+                           public Listener<const VirtualFile&>,
                            public UpdateRequester {
  public:
   static const bool FILTER = false;
 
-  GlobalDataListener(const data::Address& address =
+  DataListener(const data::Address& address =
                      data::Address::default_instance(),
-                     bool isGlobal = false)
+                     bool is = false)
       : filterDupes_(false),
         data_(data::emptyEditable<Proto>()),
         address_(address),
-        isGlobal_(isGlobal) {
+        is_(is) {
   }
-  virtual ~GlobalDataListener() {}
+  virtual ~DataListener() {}
 
   virtual void requestUpdates() {
-    if (isGlobal_)
+    if (is_)
       setData(data::editable<Proto>());
-#if 0
     else
       data::editable<VirtualFile>()->addListener(this);
-#endif
   }
 
   virtual void operator()(const Proto& p) { updateValue(p, true); }
+  virtual void operator()(const VirtualFile& f) {
+    setData(file::empty(f) ? data::emptyEditable<Proto>() :
+            data::editable<Proto>(f));
+  }
 
   data::TypedEditable<Proto>* data() const { Lock l(lock()); return data_; }
 
@@ -60,32 +63,8 @@ class GlobalDataListener : public Listener<const Proto&>,
   data::TypedEditable<Proto>* data_;
   Proto proto_;
   const data::Address address_;
-  const bool isGlobal_;
+  const bool is_;
 
-  DISALLOW_COPY_AND_ASSIGN(GlobalDataListener);
-};
-
-// DataListener listens to changes in persistent data.
-
-template <typename Proto>
-class DataListener : public GlobalDataListener<Proto>,
-                     public Listener<const VirtualFile&> {
- public:
-  DataListener(const data::Address& addr = data::Address::default_instance())
-      : GlobalDataListener<Proto>(addr) {
-  }
-  virtual ~DataListener() {}
-
-  virtual void requestUpdates() {
-    data::editable<VirtualFile>()->addListener(this);
-  }
-
-  virtual void operator()(const VirtualFile& f) {
-    setData(file::empty(f) ? data::emptyEditable<Proto>() :
-            data::editable<Proto>(f));
-  }
-
- private:
   DISALLOW_COPY_AND_ASSIGN(DataListener);
 };
 
@@ -94,7 +73,7 @@ class DataListener : public GlobalDataListener<Proto>,
 //
 
 template <typename Proto>
-void GlobalDataListener<Proto>::updateValue(const Proto& p, bool perhapsFilter) {
+void DataListener<Proto>::updateValue(const Proto& p, bool perhapsFilter) {
   {
     Lock l(lock());
     if (perhapsFilter && filterDupes_ && data::equals(proto_, p))
@@ -106,7 +85,7 @@ void GlobalDataListener<Proto>::updateValue(const Proto& p, bool perhapsFilter) 
 }
 
 template <typename Proto>
-void GlobalDataListener<Proto>::setData(data::TypedEditable<Proto>* d) {
+void DataListener<Proto>::setData(data::TypedEditable<Proto>* d) {
   Proto p;
   {
     Lock l(lock());
