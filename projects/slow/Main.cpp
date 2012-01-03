a1763dbb91449a6f53b297d4f6d6230c93604e80
Tom Swirly <tom@swirly.com>
2012-01-03 17:10:23 -0500

    Make version number current

--- projects/slow/Main.cpp
+++ projects/slow/Main.cpp
@@ -3,6 +3,6 @@
 
 START_REC_APPLICATION(rec::slow::SlowWindow,
                       SlowGold,
-                      "0.2.24",
+                      "0.2.25",
                       rec::slow::initialize,
                       rec::slow::shutdown)
