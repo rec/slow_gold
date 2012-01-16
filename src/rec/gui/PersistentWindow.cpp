f7ea133fe6a5ee006df676f7642a5e98a5a4bb54
Tom Swirly <tom@swirly.com>
2012-01-15 20:50:53 -0500

    Tweak layout

--- src/rec/gui/PersistentWindow.cpp
+++ src/rec/gui/PersistentWindow.cpp
@@ -14,7 +14,7 @@ using namespace juce;
 static const int MIN_WIDTH = 800;
 static const int MIN_HEIGHT = 550;
 static const int MIN_X = 30;
-static const int MIN_Y = 30;
+static const int MIN_Y = 100;
 static const int X_FUDGE = 50;
 static const int Y_FUDGE = 50;
 
