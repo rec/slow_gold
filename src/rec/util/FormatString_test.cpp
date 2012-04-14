ee6de05f7ae9b7dc041db1d7b51359f3ef0e74c6
Tom Swirly <tom@swirly.com>
2012-04-14 16:34:16 -0400

    Another test for FormatHelper

--- src/rec/util/FormatString_test.cpp
+++ src/rec/util/FormatString_test.cpp
@@ -9,6 +9,15 @@ namespace {
 TEST(FormatString, Trivial) {
   FormatString s("");
   EXPECT_EQ(str(s.format()), "");
+  EXPECT_EQ(s.parts().size(), 1);
+  EXPECT_EQ(s.args().size(), 0);
+}
+
+TEST(FormatString, OnePart) {
+  FormatString s("foo");
+  EXPECT_EQ(str(s.format()), "foo");
+  EXPECT_EQ(s.parts().size(), 1);
+  EXPECT_EQ(s.args().size(), 0);
 }
 
 }  // namespace
