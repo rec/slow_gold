75e1885e6b9d3bd98d78b450084e600a1485a50d
Tom Swirly <tom@swirly.com>
2012-05-29 14:56:47 -0400

    Create zip files correctly

--- scripts/protect.sh
+++ scripts/protect.sh
@@ -1,27 +1,29 @@
 VERSION=${1:-8.0.2}
 
 BIN_PATH=/development/rec/projects/slow/Builds/MacOSX/build/Release
-APP_PATH="$BIN_PATH/SlowGold $VERSION"
+APP_PATH="SlowGold $VERSION"
+ZIP_PATH="SlowGold-$VERSION"
 POST_APP_PATH="${APP_PATH}_PreARM"
 EXTENSION=app
 ARM_FILE=/development/rec/projects/slow/SlowGold.mac.arm
 
 doZip () {
   local VARIANT=$1
-  local ZIP_FILE="$APP_PATH-$VARIANT.zip"
-
-  rm -f "$ZIP_FILE" && \
-    zip -qry9 "$ZIP_FILE" "$APP_PATH.$EXTENSION"
-  echo "Created $ZIP_FILE"
+  local ZIP_FILE="$ZIP_PATH-$VARIANT.zip"
+  cd "$BIN_PATH" && \
+    rm -f "$ZIP_FILE" && \
+    zip -qry9 "$ZIP_FILE" "$APP_PATH.$EXTENSION"/*
+  echo "Created $BIN_PATH/$ZIP_FILE"
 }
 
 protect() {
   local VARIANT=$1
   local PASSPORT=$2
+
   $PASSPORT $ARM_FILE && \
-  doZip $VARIANT && \
-  rm -Rf "$APP_PATH.$EXTENSION" && \
-  mv "$POST_APP_PATH.$EXTENSION" "$APP_PATH.$EXTENSION"
+   doZip $VARIANT && \
+   rm -Rf "$APP_PATH.$EXTENSION" && \
+   mv "$POST_APP_PATH.$EXTENSION" "$APP_PATH.$EXTENSION"
 }
 
 NEW_PASSPORT=/Applications/SoftwarePassport.new.app/Contents/MacOS/Modules/ArmCLine
