1ddc90c598f97422042759527b28edd0e8152a99
Tom Swirly <tom@swirly.com>
2012-08-07 15:00:53 +0200

    Updated include path for Windows

--- projects/tests/JuceLibraryCode/BinaryData.cpp
+++ projects/tests/JuceLibraryCode/BinaryData.cpp
@@ -1009,7 +1009,7 @@ const char* icon_png = (const char*) temp_82906af6;
 const char* getNamedResource (const char*, int&) throw();
 const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) throw()
 {
-    int hash = 0;
+    unsigned int hash = 0;
     if (resourceNameUTF8 != 0)
         while (*resourceNameUTF8 != 0)
             hash = 31 * hash + *resourceNameUTF8++;
