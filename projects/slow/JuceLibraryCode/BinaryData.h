4e2cf41bd1f9429d5f491e51aed950df8c5efc86
Tom Ritchford <tom@swirly.com>
2013-08-27 15:18:53 -0400

    Start to move binary resources.

--- projects/slow/JuceLibraryCode/BinaryData.h
+++ projects/slow/JuceLibraryCode/BinaryData.h
@@ -6,6 +6,162 @@
 
 namespace BinaryData
 {
+    extern const char*   AddLoopPointButton_svg;
+    const int            AddLoopPointButton_svgSize = 21887;
+
+    extern const char*   AddLoopPointButtonDisabled_svg;
+    const int            AddLoopPointButtonDisabled_svgSize = 16373;
+
+    extern const char*   AddLoopPointButtonPressed_svg;
+    const int            AddLoopPointButtonPressed_svgSize = 18313;
+
+    extern const char*   AddLoopPointMode_svg;
+    const int            AddLoopPointMode_svgSize = 22369;
+
+    extern const char*   AddLoopPointModeDisabled_svg;
+    const int            AddLoopPointModeDisabled_svgSize = 16477;
+
+    extern const char*   DragMode_svg;
+    const int            DragMode_svgSize = 28114;
+
+    extern const char*   DragModeDisabled_svg;
+    const int            DragModeDisabled_svgSize = 22677;
+
+    extern const char*   JumpBackButton_svg;
+    const int            JumpBackButton_svgSize = 16940;
+
+    extern const char*   JumpBackButtonDisabled_svg;
+    const int            JumpBackButtonDisabled_svgSize = 14707;
+
+    extern const char*   JumpBackButtonPressed_svg;
+    const int            JumpBackButtonPressed_svgSize = 16084;
+
+    extern const char*   JumpForwardButton_svg;
+    const int            JumpForwardButton_svgSize = 16928;
+
+    extern const char*   JumpForwardButtonDisabled_svg;
+    const int            JumpForwardButtonDisabled_svgSize = 14744;
+
+    extern const char*   JumpForwardButtonPressed_svg;
+    const int            JumpForwardButtonPressed_svgSize = 16117;
+
+    extern const char*   JumpToStartButton_svg;
+    const int            JumpToStartButton_svgSize = 17757;
+
+    extern const char*   JumpToStartButtonDisabled_svg;
+    const int            JumpToStartButtonDisabled_svgSize = 14957;
+
+    extern const char*   JumpToStartButtonPressed_svg;
+    const int            JumpToStartButtonPressed_svgSize = 16698;
+
+    extern const char*   MacFolderCloseButton_svg;
+    const int            MacFolderCloseButton_svgSize = 13363;
+
+    extern const char*   MacFolderOpenButton_svg;
+    const int            MacFolderOpenButton_svgSize = 13371;
+
+    extern const char*   PlayButton_svg;
+    const int            PlayButton_svgSize = 15991;
+
+    extern const char*   PlayButtonDisabled_svg;
+    const int            PlayButtonDisabled_svgSize = 14281;
+
+    extern const char*   SetTimeMode_svg;
+    const int            SetTimeMode_svgSize = 24685;
+
+    extern const char*   SetTimeModeDisabled_svg;
+    const int            SetTimeModeDisabled_svgSize = 19396;
+
+    extern const char*   StopButton_svg;
+    const int            StopButton_svgSize = 15093;
+
+    extern const char*   StopButtonDisabled_svg;
+    const int            StopButtonDisabled_svgSize = 14201;
+
+    extern const char*   ZoomInCursor_svg;
+    const int            ZoomInCursor_svgSize = 17598;
+
+    extern const char*   ZoomMode_svg;
+    const int            ZoomMode_svgSize = 19762;
+
+    extern const char*   ZoomMoveDisabled_svg;
+    const int            ZoomMoveDisabled_svgSize = 30124;
+
+    extern const char*   ZoomOutButton_svg;
+    const int            ZoomOutButton_svgSize = 39902;
+
+    extern const char*   ZoomOutButtonDisabled_svg;
+    const int            ZoomOutButtonDisabled_svgSize = 39634;
+
+    extern const char*   ZoomOutButtonPressed_svg;
+    const int            ZoomOutButtonPressed_svgSize = 39707;
+
+    extern const char*   ZoomToSelectionButton_svg;
+    const int            ZoomToSelectionButton_svgSize = 30800;
+
+    extern const char*   ZoomToSelectionButtonDisabled_svg;
+    const int            ZoomToSelectionButtonDisabled_svgSize = 17720;
+
+    extern const char*   ZoomToSelectionButtonPressed_svg;
+    const int            ZoomToSelectionButtonPressed_svgSize = 22883;
+
+    extern const char*   deremovedTranslatedStrings_def;
+    const int            deremovedTranslatedStrings_defSize = 15;
+
+    extern const char*   detranslated_txt;
+    const int            detranslated_txtSize = 19020;
+
+    extern const char*   deTranslatedStrings_def;
+    const int            deTranslatedStrings_defSize = 62004;
+
+    extern const char*   deuntranslated_txt;
+    const int            deuntranslated_txtSize = 0;
+
+    extern const char*   enincomingTranslatedStrings_def;
+    const int            enincomingTranslatedStrings_defSize = 32049;
+
+    extern const char*   enremovedTranslatedStrings_def;
+    const int            enremovedTranslatedStrings_defSize = 15;
+
+    extern const char*   enTranslatedStrings_def;
+    const int            enTranslatedStrings_defSize = 36046;
+
+    extern const char*   esremovedTranslatedStrings_def;
+    const int            esremovedTranslatedStrings_defSize = 15;
+
+    extern const char*   estranslated_txt;
+    const int            estranslated_txtSize = 19677;
+
+    extern const char*   esTranslatedStrings_def;
+    const int            esTranslatedStrings_defSize = 62451;
+
+    extern const char*   esuntranslated_txt;
+    const int            esuntranslated_txtSize = 0;
+
+    extern const char*   frremovedTranslatedStrings_def;
+    const int            frremovedTranslatedStrings_defSize = 15;
+
+    extern const char*   frtranslated_txt;
+    const int            frtranslated_txtSize = 20532;
+
+    extern const char*   frTranslatedStrings_def;
+    const int            frTranslatedStrings_defSize = 65188;
+
+    extern const char*   fruntranslated_txt;
+    const int            fruntranslated_txtSize = 0;
+
+    extern const char*   idremovedTranslatedStrings_def;
+    const int            idremovedTranslatedStrings_defSize = 15;
+
+    extern const char*   idtranslated_txt;
+    const int            idtranslated_txtSize = 17634;
+
+    extern const char*   idTranslatedStrings_def;
+    const int            idTranslatedStrings_defSize = 59202;
+
+    extern const char*   iduntranslated_txt;
+    const int            iduntranslated_txtSize = 0;
+
     extern const char*   bigicon_png;
     const int            bigicon_pngSize = 67899;
 
