53d50d34682910ed91d243b1b89f89550611218f
Tom Ritchford <tom@swirly.com>
2013-09-18 18:56:36 -0400

    start to clean up Color.\*

--- src/rec/gui/Color.h
+++ src/rec/gui/Color.h
@@ -8,45 +8,10 @@ namespace rec {
 namespace gui {
 namespace color {
 
-Colour get(int i);
 Colour get(const Colors& colors, int index);
-void prepare(const Colors& colors, juce::Graphics* g);
 const Colors& getDefaultColors();
 
 Colour makeColour(const Color& color);
-uint32 makeARGB(const Color& c);
-uint32 nameToARGB(const string& name, const juce::Colour& dflt);
-
-class ColorArg {
- public:
-  ColorArg(uint32 c) : argb_(c) {}
-  ColorArg(const Colour& c) : argb_(c.getARGB()) {}
-  ColorArg(const Color& c) : argb_(makeARGB(c)) {}
-  ColorArg(const string& s) : argb_(nameToARGB(s, juce::Colour())) {}
-
-  operator Colour() const { return Colour(argb_); }
-  operator uint32() const { return argb_; }
-  operator Color() const {
-    Color c;
-    c.set_argb(argb_);
-    return c;
-  }
-
- private:
-  uint32 argb_;
-};
-
-typedef const ColorArg& CCA;
-
-Colors makeColors();
-Colors makeColors(CCA);
-Colors makeColors(CCA, CCA);
-Colors makeColors(CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA, CCA, CCA, CCA);
 
 }  // namespace color
 }  // namespace gui
