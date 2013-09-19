6a4fddd3caab0ae797e95afdca041ca08da578ec
Tom Ritchford <tom@swirly.com>
2013-09-18 20:20:35 -0400

    More cleaning up colors.

--- src/rec/gui/Color.cpp
+++ src/rec/gui/Color.cpp
@@ -7,6 +7,39 @@ namespace rec {
 namespace gui {
 namespace color {
 
+namespace {
+
+std::pair<Colour*, Colour*> getColours() {
+  static Colour COLORS[] = {
+    Colours::white,
+    Colours::lightblue,
+    Colours::seagreen,
+    juce::Colour(0xffd3d3d3),
+    juce::Colour(0xff809090),
+    juce::Colours::red,
+    juce::Colours::black,
+    juce::Colour(0xff607070),
+    // Colours::grey,
+    // Colours::blue, //grey
+  };
+  return std::make_pair(COLORS, COLORS + arraysize(COLORS));
+}
+
+Colors getDefaultInternal() {
+  Colors colors;
+  std::pair<Colour*, Colour*> colours = getColours();
+  for (Colour* c = colours.first; c != colours.second; ++c)
+    colors.add_color()->set_argb(c->getARGB());
+  return colors;
+};
+
+const Colors& getDefaultColors() {
+  static Colors colors = getDefaultInternal();
+  return colors;
+}
+
+}  // namespace
+
 uint32 nameToARGB(const string& name, const juce::Colour& dflt) {
   return juce::Colours::findColourForName(str(name), dflt).getARGB();
 }
@@ -32,22 +65,6 @@ Colour makeColour(const Color& color) {
   return Colour(makeARGB(color));
 }
 
-static std::pair<Colour*, Colour*> getColours() {
-  static Colour COLORS[] = {
-    Colours::white,
-    Colours::lightblue,
-    Colours::seagreen,
-    juce::Colour(0xffd3d3d3),
-    juce::Colour(0xff809090),
-    juce::Colours::red,
-    juce::Colours::black,
-    juce::Colour(0xff607070),
-    // Colours::grey,
-    // Colours::blue, //grey
-  };
-  return std::make_pair(COLORS, COLORS + arraysize(COLORS));
-}
-
 Colour get(int i) {
   std::pair<Colour*, Colour*> colours = getColours();
   if (i < colours.second - colours.first)
@@ -58,18 +75,6 @@ Colour get(int i) {
 };
 
 
-static Colors getDefaultInternal() {
-  Colors colors;
-  std::pair<Colour*, Colour*> colours = getColours();
-  for (Colour* c = colours.first; c != colours.second; ++c)
-    colors.add_color()->set_argb(c->getARGB());
-  return colors;
-};
-
-const Colors& getDefaultColors() {
-  static Colors colors = getDefaultInternal();
-  return colors;
-}
 
 Colour get(const Colors& colors, int i) {
   return i < colors.color_size() ? makeColour(colors.color(i)) : get(i);
@@ -78,91 +83,6 @@ Colour get(const Colors& colors, int i) {
 void prepare(const Colors& colors, Graphics* g) {
   g->fillAll(get(colors, 0));
   g->setColour(get(colors, 1));
-}
-
-static void push(const ColorArg& colorArg, Colors* colors) {
-  colors->add_color()->set_argb(colorArg);
-}
-
-Colors makeColors() {
-  Colors colors;
-  return colors;
-}
-
-Colors makeColors(CCA a1) {
-  Colors colors;
-  push(a1, &colors);
-  return colors;
-}
-
-Colors makeColors(CCA a1, CCA a2) {
-  Colors colors;
-  push(a1, &colors);
-  push(a2, &colors);
-  return colors;
-}
-
-Colors makeColors(CCA a1, CCA a2, CCA a3) {
-  Colors colors;
-  push(a1, &colors);
-  push(a2, &colors);
-  push(a3, &colors);
-  return colors;
-}
-
-Colors makeColors(CCA a1, CCA a2, CCA a3, CCA a4) {
-  Colors colors;
-  push(a1, &colors);
-  push(a2, &colors);
-  push(a3, &colors);
-  push(a4, &colors);
-  return colors;
-}
-
-Colors makeColors(CCA a1, CCA a2, CCA a3, CCA a4, CCA a5) {
-  Colors colors;
-  push(a1, &colors);
-  push(a2, &colors);
-  push(a3, &colors);
-  push(a4, &colors);
-  push(a5, &colors);
-  return colors;
-}
-
-Colors makeColors(CCA a1, CCA a2, CCA a3, CCA a4, CCA a5, CCA a6) {
-  Colors colors;
-  push(a1, &colors);
-  push(a2, &colors);
-  push(a3, &colors);
-  push(a4, &colors);
-  push(a5, &colors);
-  push(a6, &colors);
-  return colors;
-}
-
-Colors makeColors(CCA a1, CCA a2, CCA a3, CCA a4, CCA a5, CCA a6, CCA a7) {
-  Colors colors;
-  push(a1, &colors);
-  push(a2, &colors);
-  push(a3, &colors);
-  push(a4, &colors);
-  push(a5, &colors);
-  push(a6, &colors);
-  push(a7, &colors);
-  return colors;
-}
-
-Colors makeColors(CCA a1, CCA a2, CCA a3, CCA a4, CCA a5, CCA a6, CCA a7, CCA a8) {
-  Colors colors;
-  push(a1, &colors);
-  push(a2, &colors);
-  push(a3, &colors);
-  push(a4, &colors);
-  push(a5, &colors);
-  push(a6, &colors);
-  push(a7, &colors);
-  push(a8, &colors);
-  return colors;
 }
 
 Colour makeColour(const Color& c, const ColourMap& map) {
--- src/rec/gui/Color.h
+++ src/rec/gui/Color.h
@@ -14,46 +14,12 @@ typedef std::unordered_map<string, Colour> ColourMap;
 
 Colour get(const Colors& colors, int index);
 
-Colour get(int i);
-Colour get(const Colors& colors, int index);
 void prepare(const Colors& colors, juce::Graphics* g);
-const Colors& getDefaultColors();
 
 uint32 makeARGB(const Color& c);
 uint32 nameToARGB(const string& name, const juce::Colour& dflt);
 
 Colour makeColour(const Color&, const ColourMap&);
-
-class ColorArg {
- public:
-  ColorArg(uint32 c) : argb_(c) {}
-  ColorArg(const Colour& c) : argb_(c.getARGB()) {}
-  ColorArg(const Color& c) : argb_(makeARGB(c)) {}
-  ColorArg(const string& s) : argb_(nameToARGB(s, juce::Colour())) {}
-
-  operator Colour() const { return Colour(argb_); }
-  operator uint32() const { return argb_; }
-  operator Color() const {
-    Color c;
-    c.set_argb(argb_);
-    return c;
-  }
-
- private:
-  uint32 argb_;
-};
-
-typedef const ColorArg& CCA;
-
-Colors makeColors();
-Colors makeColors(CCA);
-Colors makeColors(CCA, CCA);
-Colors makeColors(CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA, CCA, CCA);
-Colors makeColors(CCA, CCA, CCA, CCA, CCA, CCA, CCA, CCA);
 
 }  // namespace color
 }  // namespace gui
