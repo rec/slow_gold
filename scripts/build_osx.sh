2a0aed42584bb8f11405da57610024cabc4e3208
Tom Ritchford <tom@swirly.com>
2017-01-25 14:33:36 +0100

    Simple script to build on OS/X

---
+++ scripts/build_osx.sh
@@ -0,0 +1,14 @@
+ROOT=/development/rec/projects/slow/Builds/MacOSX
+CONFIG=Release
+VERSION=8.1.0
+
+BUILD_DIR=build/$CONFIG
+
+BINARY=$BUILD_DIR/SlowGold\ 8.app
+VERSION_BINARY=$BUILD_DIR/SlowGold\ $VERSION.app
+
+cd $ROOT && \
+    xcodebuild -project SlowGold.xcodeproj -configuration $CONFIG && \
+    mv "$BINARY" "$VERSION_BINARY" && \
+    zip -r "$VERSION_BINARY.zip" "$VERSION_BINARY" && \
+    open $BUILD_DIR
